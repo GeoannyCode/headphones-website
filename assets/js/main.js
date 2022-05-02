020a143394206a1b05932dc5e05b86684412ac9b
Geovannidev <diegobracero@hotmail.com>
2022-05-01 21:43:21 -0500

    Home & buttons

--- assets/js/main.js
+++ assets/js/main.js
@@ -19,7 +19,15 @@ if(navClose){
 }
 
 /*=============== REMOVE MENU MOBILE ===============*/
+const navLink = document.querySelectorAll('.nav__link')
 
+function linkAction(){
+    const navMenu = document.getElementById('nav-menu')
+
+    navMenu.classList.remove('show-menu')
+}
+
+navLink.forEach(n=> n.addEventListener('click', linkAction))
 
 /*=============== CHANGE BACKGROUND HEADER ===============*/
 
