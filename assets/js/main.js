13dd4aa72b6324ad2b7a2e6e317418c8681953e2
Geovannydev <diegobracero@hotmail.com>
2022-05-03 17:41:15 -0500

    Change background header | Sponsor | Specs

--- assets/js/main.js
+++ assets/js/main.js
@@ -30,8 +30,12 @@ function linkAction(){
 navLink.forEach(n=> n.addEventListener('click', linkAction))
 
 /*=============== CHANGE BACKGROUND HEADER ===============*/
+function scrollHeader(){
+    const header = document.getElementById('header')
+    if(this.scrollY >= 25) header.classList.add('scroll-header'); else header.classList.remove('scroll-header')    
+}
 
-
+window.addEventListener('scroll',scrollHeader)
 /*==================== SHOW SCROLL UP ====================*/ 
 
 
