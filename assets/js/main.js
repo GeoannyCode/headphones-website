b5e0f7c317a554b4d036186148fd664bb3405c4e
geovannydev <diegobracero@hotmail.com>
2022-05-14 21:29:28 -0500

    scrollup

--- assets/js/main.js
+++ assets/js/main.js
@@ -36,8 +36,14 @@ function scrollHeader(){
 }
 
 window.addEventListener('scroll',scrollHeader)
-/*==================== SHOW SCROLL UP ====================*/ 
 
+/*==================== SHOW SCROLL UP ====================*/ 
+function scrollUp(){
+    const scrollUp = document.getElementById('scroll-up');
+    if(this.scrollY >= 200) scrollUp.classList.add('show-scroll');else scrollUp.classList.remove('show-scroll');
+}
+
+window.addEventListener('scroll',scrollUp)
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
 
