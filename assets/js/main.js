79aaa03bf79cf389f4537d15d0f24e404006f347
geovannydev <diegobracero@hotmail.com>
2022-05-15 14:11:31 -0500

    Animation

--- assets/js/main.js
+++ assets/js/main.js
@@ -65,5 +65,22 @@ function scrollActive(){
 }
 
 window.addEventListener('scroll', scrollActive)
+
 /*=============== SCROLL REVEAL ANIMATION ===============*/
 
+const sr = ScrollReveal({
+    distance: '60px',
+    duration: 2500,
+    delay: 400,
+    // reset: true
+})
+
+sr.reveal(`.home__header, .section__title`, {delay:600})
+sr.reveal(`.home__footer`, {delay:700})
+sr.reveal(`.home__img`, {delay: 900, origin: 'top'})
+
+sr.reveal(`.sponsor__img, .products__card, .footer__logo, .footer__content, .footer__copy`, {origin: 'top', interval: 100})
+sr.reveal(`.specs__data, .discount__animate`,{origin: 'left', interval: 100})
+sr.reveal(`.specs__img, .discount__img`, {origin: 'right'})
+sr.reveal(`.case__img`, {origin: 'top'})
+sr.reveal(`.case__data`, {origin: 'top'})
